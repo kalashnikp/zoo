37270b2a03fcf2c39acb7eba53fa64e66a7d863b
Pavel Kalashnik <pakalashnik@gmail.com>
2023-04-01 20:26:35 +0300

    01/04/

--- instances.py
+++ instances.py
@@ -1,21 +1,19 @@
 from animal import Animal
 
 class Bird(Animal):
-    '''Наследовательский класс птицы / The bird's research class'''
+    '''The bird research class'''
 
-    def __init__(self, energy=2, weight=3, max_age=3, name='Bird'):
+    def __init__(self, energy=4, weight=4, max_age=3, name='Bird'):
         super().__init__(energy, weight, max_age, name)
 
     def walk(self):
         super().walk()
         print(f'{self.name} is flying\n')
 
-    
-
 class Fish(Animal):
-    '''Наследовательский класс рыбы / The research class of fish'''
+    '''The research class of fish'''
 
-    def __init__(self, energy=3, weight=2, max_age=3, name='Fish'):
+    def __init__(self, energy=3, weight=3, max_age=3, name='Fish'):
         super().__init__(energy, weight, max_age, name)
 
     def walk(self):
@@ -23,9 +21,9 @@ class Fish(Animal):
         print(f'{self.name} is swimming\n')
 
 class Dog(Animal):
-    '''Наследовательский класс животного / The dog's research class '''
+    '''The Dog research class'''
 
-    def __init__(self, energy=4, weight=5, max_age=5, name='Dog'):
+    def __init__(self, energy=5, weight=5, max_age=3, name='Dog'):
         super().__init__(energy, weight, max_age, name)
 
     def walk(self):
@@ -33,12 +31,3 @@ class Dog(Animal):
         print(f'{self.name} is running\n')
 
 
-# elf = Fish(5, 2, 7, 'ed')
-
-# elf.sleep()
-# elf.walk()
-# elf.eat()
-
-# print(elf.energy)
-# print(elf.age)
-# print(elf.weight)
---
+++ nature_reserce.py
@@ -0,0 +1,91 @@
+from animal import Animal
+from instances import Bird, Fish, Dog
+import random
+
+
+
+class NatureReverce():
+    '''Zoo class'''
+    
+    global zoo
+    global dead_animals
+    global new_zoo
+
+    def __init__(self) -> None:
+        pass
+
+    zoo = [
+        Bird(),
+        Bird(),
+        Fish(),
+        Fish(),
+        Dog(),
+        Dog(),
+        Animal(random.randint(1, 5), random.randint(1, 5), random.randint(1, 5), 'Animal'),
+        Animal(random.randint(1, 5), random.randint(1, 5), random.randint(1, 5), 'Animal')        
+    ]
+
+    dead_animals = []
+    new_zoo = []
+    
+    def is_dead(self):
+        '''Animal health check function'''
+
+        for item in zoo:
+            item.is_too_old() # is_too_old check
+            
+            if item.energy <= 0 or item.weight <=0 or item.is_too_old == True:
+                zoo.remove(item)
+                dead_animals.append(item)
+
+    def do_something(self):
+        '''Calling the animals functions'''
+
+        for item in zoo:
+            item.sleep()
+            item.eat()
+            item.walk()
+            item.try_increment_age()
+
+    def new_amnimal(self):
+        '''To produce offspring function'''
+        global zoo
+        for items in (zoo):
+            a = random.choices([1, 0], weights=[0.25, 0.75])
+            b = int(''.join(map(str,a)))
+            c = bool(b)
+    
+            if c == True:
+                new_zoo.append(items)
+
+        zoo += new_zoo
+
+
+
+
+
+
+
+my_zoo = NatureReverce()
+my_zoo.do_something()
+my_zoo.is_dead()
+my_zoo.new_amnimal()
+
+print(f'Животные зоопарка:')
+for item in zoo:
+    print(f'{item.name}')
+
+print(f'Умершие животные:')
+for item in dead_animals:
+    print(f'{item.name} is dead')
+
+print(f'Рожденные животные:')
+for item in new_zoo:
+    print(f'{item.name} is new born')
+    
+
+
+
+
+
+
