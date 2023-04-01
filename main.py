384279bae3fee488096f2b35c124b0b6eab77674
P <pakalashnik@gmail.com>
2023-04-02 00:37:27 +0300

    123

--- main.py
+++ main.py
@@ -1,35 +1,6 @@
 from animal import Animal
 from instances import Bird, Fish, Dog
-<<<<<<< HEAD
 from nature_reserce import NatureReverce, zoo
-=======
-from nature_reserce import NatureReverse, zoo
-
-
-
-
-def life_circle():
-    global my_zoo
-    my_zoo = NatureReverse() 
-    my_zoo.new_zoo()       
-    my_zoo.do_something()
-
-def run():
-    i = 0
-    iterations = int(input(f'Введите количество циклов'))
-    while i < iterations:
-        life_circle()
-        i += 1
-    
-def output():
-    for item in my_zoo.zoo:
-        print(f'we have a {item.name}\nhe has a {item.energy} energy\nhe has a {item.weight} weight \nhe a {item.age} years old\n')      
-
-
-
-run()
-output()
->>>>>>> ee6e0e38954baad2dd75581bb7246d7e93fd6745
 
 
 
